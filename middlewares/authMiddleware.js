0a611ca7ac3d752e034030806ac8c77ef7357472
Raouf Satto <raoufsato999@gmail.com>
2021-06-26 06:21:51 +0300

    add middleware to protect our routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,7 @@
+const User = require("../models/User");
+module.exports = (req, res, next) => {
+	User.findById(req.session.userID, (err, user) => {
+		if (err || !user) return res.redirect("/login");
+		next();
+	});
+};
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const pageController = require("../controllers/pageControllers");
-
+const redirectMiddleware = require("../middlewares/redirectMiddleware");
 const router = express.Router();
 
 router.route("/").get(pageController.getIndexPage);
 router.route("/about").get(pageController.getAboutPage);
-router.route("/register").get(pageController.getRegisterPage);
-router.route("/login").get(pageController.getLoginPage);
+router
+	.route("/register")
+	.get(redirectMiddleware, pageController.getRegisterPage);
+router.route("/login").get(redirectMiddleware, pageController.getLoginPage);
 router.route("/contact").get(pageController.getContacPage);
 
 module.exports = router;
